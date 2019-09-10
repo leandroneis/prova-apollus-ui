2604def556feea59a5f1f35f8cd353c11fd02c56
Leandro Gustavo Neis <lgn200@gmail.com>
2019-09-10 02:57:59 -0300

    preparando para heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://prova-apollus-usuario-api.herokuapp.com',
+  apiUrl: 'https://prova-apollus-api.herokuapp.com',
 
-  tokenWhitelistedDomains: [ new RegExp('prova-apollus-usuario-api.herokuapp.com') ],
+  tokenWhitelistedDomains: [ new RegExp('prova-apollus-api.herokuapp.com') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
